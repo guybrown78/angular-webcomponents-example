8bcdbaac1a9eff8d99d5ec87b1c4ab6b5c4de0aa
Guy Brown <guy@digitalopolis.co.uk>
2020-05-07 16:34:30 +0100

    improved tables

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -6,10 +6,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-
+	data = [
+		{
+			id: 1,
+			status: "LIVE",
+			code: "CS_001",
+			name: "Cold Water Survival",
+			registered:164
+		},
+		{
+			id: 2,
+			status: "LIVE",
+			code: "CS_020",
+			name: "Optito Bosiet with CA-EBS",
+			registered:98
+		},
+		{
+			id: 3,
+			status: "LIVE",
+			code: "CS_300",
+			name: "Course Three",
+			registered:12
+		},
+		{
+			id: 4,
+			status: "LIVE",
+			code: "CS_444",
+			name: "Course Four",
+			registered:564
+		},
+		{
+			id: 5,
+			status: "ARCHIVED",
+			code: "CS_505",
+			name: "Course Five",
+			registered:null
+		},
+		{
+			id: 6,
+			status: "LIVE",
+			code: "CS_666",
+			name: "Course Six",
+			registered:10666
+		}
+	];
   constructor() { }
 
   ngOnInit() {
-  }
+	}
+	
+	onCourseActionSelected(event){
+		const details = event.detail;
+		const courseId = Number(details.dropdownId);
+		console.log(`onCourseActionSelected - Action selected for course ${courseId}. Action value selected is ${details.value}`);
+		console.log(event, courseId, details);
+	}
 
 }
