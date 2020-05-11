8141440f73ab86844fd2d612ed024c2c1aa69b05
Guy Brown <guy@digitalopolis.co.uk>
2020-05-11 15:48:30 +0100

    added quicklinks

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,11 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+	}
+	
+	onQuicklinkOptionSelected(event){
+		const { value } = event.detail;
+		console.log(`onQuicklinkOptionSelected - singular dropdown event emiited from the quicklink dropdown component. This can be used to call methods within a component, goto routes etc. Value selected is ${value}`)
+	}
 
 }
