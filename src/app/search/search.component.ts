e9e069053dc36eea2b8b94005e76958dbba9282b
Guy Brown <guy@digitalopolis.co.uk>
2020-07-01 19:38:38 +0100

    serach input

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import countries from './countries'
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+	countryList = [ ...countries ];
+	resultsList = [];
+  constructor() {
+	 }
 
   ngOnInit() {
   }
 
+	onSearchInput(event){
+		const value = event.detail.value.toLowerCase();
+		this.resultsList = value.length > 1 
+			// if value has 2 or more chars then search
+			? [...this.countryList.filter(c => c.name.toLowerCase().includes(value))]
+			// else don't bother
+			:[];
+	}
+
+	onSearchSelected(event){
+		console.log("SELECTED", event.detail.value);
+		this.resultsList = [];
+	}
 }
